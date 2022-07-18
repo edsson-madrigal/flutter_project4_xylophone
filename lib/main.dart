d9d736d5af8799b3e436a553a2cf5b1e1258e120
Edsson <akairu@hotmail.com>
2022-07-17 21:17:02 -0500

    refactor code

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,12 @@ void main() => runApp(XylophoneApp());
 class XylophoneApp extends StatelessWidget {
   const XylophoneApp({Key? key}) : super(key: key);
 
+  void playSound(int soundNumber) {
+    Audio.load('assets/note$soundNumber.wav')
+      ..play()
+      ..dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -19,63 +25,49 @@ class XylophoneApp extends StatelessWidget {
               ElevatedButton(
                 style: ElevatedButton.styleFrom(primary: Colors.red),
                 onPressed: () {
-                  Audio.load('assets/note1.wav')
-                    ..play()
-                    ..dispose();
+                  playSound(1);
                 },
                 child: Text(''),
               ),
               ElevatedButton(
                 style: ElevatedButton.styleFrom(primary: Colors.orange),
                 onPressed: () {
-                  Audio.load('assets/note2.wav')
-                    ..play()
-                    ..dispose();
+                  playSound(2);
                 },
                 child: Text(''),
               ),
               ElevatedButton(
                 style: ElevatedButton.styleFrom(primary: Colors.yellow),
                 onPressed: () {
-                  Audio.load('assets/note3.wav')
-                    ..play()
-                    ..dispose();
+                  playSound(3);
                 },
                 child: Text(''),
               ),
               ElevatedButton(
                 style: ElevatedButton.styleFrom(primary: Colors.teal),
                 onPressed: () {
-                  Audio.load('assets/note4.wav')
-                    ..play()
-                    ..dispose();
+                  playSound(4);
                 },
                 child: Text(''),
               ),
               ElevatedButton(
                 style: ElevatedButton.styleFrom(primary: Colors.blue),
                 onPressed: () {
-                  Audio.load('assets/note5.wav')
-                    ..play()
-                    ..dispose();
+                  playSound(5);
                 },
                 child: Text(''),
               ),
               ElevatedButton(
                 style: ElevatedButton.styleFrom(primary: Colors.purple),
                 onPressed: () {
-                  Audio.load('assets/note6.wav')
-                    ..play()
-                    ..dispose();
+                  playSound(6);
                 },
                 child: Text(''),
               ),
               ElevatedButton(
                 style: ElevatedButton.styleFrom(primary: Colors.green),
                 onPressed: () {
-                  Audio.load('assets/note7.wav')
-                    ..play()
-                    ..dispose();
+                  playSound(7);
                 },
                 child: Text(''),
               ),
